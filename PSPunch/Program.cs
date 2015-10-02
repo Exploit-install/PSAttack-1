2d6c6ee11db19378bf82e8d8671858a84ff85809
Jared Haight <jared@photosandtext.com>
2015-10-02 16:42:49 -0400

    Copied reference shell from Benx0a's awesomer shell.

--- PSPunch/Program.cs
+++ PSPunch/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 
 namespace PSPunch
 {
@@ -10,6 +9,27 @@ namespace PSPunch
     {
         static void Main(string[] args)
         {
+            Console.Write("aps> ");
+            String cmd = Console.ReadLine();
+
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(cmd);
+
+            Collection<PSObject> output = ps.Invoke();
+            if (output != null)
+            {
+                foreach (PSObject rtnItem in output)
+                {
+                    Console.WriteLine(rtnItem.ToString());
+                }
+            }
+            rs.Close();
+            Console.Write("Press any key to exit.");
+            Console.ReadLine();
         }
     }
 }
