edd72f305f52ae75a8c4ccd3b07a3540d08231b0
Jared Haight <jared@photosandtext.com>
2015-12-02 20:38:19 -0500

    Cleaning stuff up

--- PSPunch/Program.cs
+++ PSPunch/Program.cs
@@ -13,15 +13,37 @@ namespace PSPunch
     {
         static PunchInput PSInit()
         {
+
+            //Setup PS Host and runspace
             PunchInput punchInput = new PunchInput();
             punchInput.host = new PSPunchHost();
             Runspace runspace = RunspaceFactory.CreateRunspace(punchInput.host);
             runspace.Open();
             punchInput.runspace = runspace;
+
+            //Decrypt modules
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            Stream moduleStream = assembly.GetManifestResourceStream("PSPunch.Modules.invoke-mimikatz.ps1.enc");
+            ImportModules(punchInput, moduleStream);
+            
+            // Setup Console stage 1
+            string prompt = "PSPUNCH! #> ";
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
+
+            //Setup PS env
             punchInput.cmd = "set-executionpolicy bypass -Scope process -Force";
             Processing.PSExec(punchInput);
             punchInput.cmd = "function Test-Admin { $wid = [System.Security.Principal.WindowsIdentity]::GetCurrent(); $prp = New-Object System.Security.Principal.WindowsPrincipal($wid); $adm = [System.Security.Principal.WindowsBuiltInRole]::Administrator; $prp.IsInRole($adm);}; write-host 'Is Admin: '(test-admin)";
             punchInput = Processing.PSExec(punchInput);
+
+            //Setup Console
+            punchInput.loopPos = 0;
+            punchInput.cmdComplete = false;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(prompt);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
             return punchInput;
 
         }
@@ -48,18 +70,7 @@ namespace PSPunch
         static void Main(string[] args)
         {
             string prompt = "PSPUNCH! #> ";
-            Console.BackgroundColor = ConsoleColor.DarkBlue;
-            Console.Clear();
             PunchInput punchInput = PSInit();
-            Console.WriteLine(punchInput.output);
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            Stream moduleStream = assembly.GetManifestResourceStream("PSPunch.Modules.invoke-mimikatz.ps1.enc");
-            ImportModules(punchInput, moduleStream);
-            punchInput.loopPos = 0;
-            punchInput.cmdComplete = false;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.Write(prompt);
-            Console.ForegroundColor = ConsoleColor.Yellow;
             while (true)
             {
                 punchInput.keyInfo = Console.ReadKey();
