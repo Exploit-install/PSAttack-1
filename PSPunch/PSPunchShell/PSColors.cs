885b9ffedafa95946d1e79243980524e21006611
Jared Haight <jared@photosandtext.com>
2015-12-17 08:38:07 -0500

    Seperated colors out to PSColors.cs for central management.

---
+++ PSPunch/PSPunchShell/PSColors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PSPunch.PSPunchShell
+{
+    class PSColors
+    {
+        public static ConsoleColor background = ConsoleColor.DarkBlue;
+        public static ConsoleColor prompt = ConsoleColor.White;
+        public static ConsoleColor cmdText = ConsoleColor.Green;
+        public static ConsoleColor normalText = ConsoleColor.Gray;
+        public static ConsoleColor paramText = ConsoleColor.DarkGray;
+        public static ConsoleColor warningText = ConsoleColor.Yellow;
+        public static ConsoleColor errorText = ConsoleColor.Red;
+        public static ConsoleColor debugText = ConsoleColor.DarkGray;
+    }
+}
